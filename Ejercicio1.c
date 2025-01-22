d764af77c3216ddf1078224ca30969229f18f881
daviz616 <david.castejon616@gmail.com>
2025-01-22 14:14:13 +0100

    leo marescutti

--- Ejercicio1.c
+++ Ejercicio1.c
@@ -2,36 +2,49 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 // Función de comparación para qsort
+void ordenarnums(int * arr3, int n2){
+    int cambio;
+    for (int i = 0; i < n2 ; i++){
+        for (int j = 0; j < n2-1 ; j++){
+            if(arr3[i] > arr3[j]){
+            cambio == arr3[i];
+            arr3[i] == arr3[j];
+            arr3[j] == cambio;
+            }
+        }
+    }
+}
 
 
 // Función para imprimir un array
-void printArray(int *arr, int n) {
+void printArray(int *arr1, int n) {
     for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+        printf("%d ", arr1[i]);
     }
     printf("\n");
 }
 
 int main(int argc, char *argv[]) {
-    int n;
-    int *array;
-
-  array = (int *)malloc(n * sizeof(int));
-    if (array == NULL) {
+    int n1;
+    int * arr2;
+    
+  arr2 = (int *)malloc(n1 * sizeof(int));
+    if (arr2 == NULL) {
         printf("Error al reservar memoria");
         return 1;
     }
 
     if (argc == 1) {
-        n = atoi(argv[1]); // Convertir el primer argumento a entero
+        n1 = atoi(argv[1]); // Convertir el primer argumento a entero
     } else {
         // Pedir al usuario el número de enteros
         printf("Introduce el número de enteros que quieres ordenar: ");
-        scanf("%d", &n);
+        scanf("%d", &n1);
     }
 
-    if (n <= 0) {
+    if (n1 <= 0) {
         printf("Dato introducido no valido, introduce un entero mayor que cero.\n");
         return 1;
     }
@@ -39,25 +52,26 @@ int main(int argc, char *argv[]) {
     // Leer valores del usuario o de los argumentos de línea de comandos
     if (argc == 2) {
         // Leer valores desde los argumentos de línea de comandos
-        if (argc < n) {
+        if (argc < n1) {
             printf("Datos introducidos no validos, introduce solo un entero");
-            free(array);
+            free(arr2);
             return 1;
         }
-        for (int i = 0; i < n; i++) {
-            array[i] = atoi(argv[2]);
+        for (int i = 0; i < n1; i++) {
+            arr2[i] = atoi(argv[2]);
         }
     } else {
         // Leer valores desde la entrada estándar
-        printf("Introduce los %d enteros:\n", n);
-        for (int i = 0; i < n; i++) {
-            scanf("%d", &array[i]);
+        printf("Introduce los %d enteros:\n", n1);
+        for (int i = 0; i < n1; i++) {
+            scanf("%d", &arr2[i]);
         }
     }
-
+    printf("Cadena ordenada");
+    ordenarnums(arr2,n1);
 
     // Liberar memoria dinámica
-    free(array);
+    free(arr2);
 
     return 0;
 }
